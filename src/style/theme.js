e57d61fc87351a9e02472bd2878226ea0a095622
igordelesposti <igordelesposti@gmail.com>
2020-04-28 17:38:34 -0300

    Estilização final da página de post

--- src/style/theme.js
+++ src/style/theme.js
@@ -1,8 +1,11 @@
 import { createMuiTheme } from "@material-ui/core";
 import blue from "@material-ui/core/colors/blue";
+import orange from "@material-ui/core/colors/orange";
+
 
 export default createMuiTheme({
   palette: {
-    primary: blue
+    primary: blue,
+    secondary: orange,
   }
 });
